c1cb45e60eb09ae16ce32443c8efc5f5bed5ef90
Maxime <monillon@outlook.fr>
2022-03-04 17:14:29 +0100

    Correction fichier de connect lié à BDD de dev

--- config/database.php
+++ config/database.php
@@ -1,25 +1,25 @@
 <?php
 class Database{
- 
+
     // specify your own database credentials
-    private $host = "db5000275132.hosting-data.io";
-    private $db_name = "dbs268534";
-    private $username = "dbu280686";
-    private $password = "caWvu6-fichij-detpav";
+    private $host = "db5000548814.hosting-data.io";
+    private $db_name = "dbs526813";
+    private $username = "dbu647058";
+    private $password = "cocwug-2cybwu-kobBys";
     public $conn;
- 
+
     // get the database connection
     public function getConnection(){
- 
+
         $this->conn = 'null';
- 
+
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
-        
+
         return $this->conn;
 
     }
